cedda3e7f17732319f6db072a058444fc333792c
Ryley C <ryleychallis15@gmail.com>
2026-03-30 11:12:13 +0100

    Upload files

--- css/router.js
+++ css/router.js
@@ -24,3 +24,20 @@ document.addEventListener('click', async function(e) {
         }
     }
 });
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    
+    // Prevent scrolling when menu is open
+    document.body.classList.toggle('no-scroll');
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(n => n.addEventListener('click', () => {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}));
